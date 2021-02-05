df51a5d67c8ddfb5f715636b2e3c8bc264985d8d
Akshay <akshayyadav04031998@gmail.com>
2021-02-05 13:23:49 +0530

    myblog

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from home import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('home', views.home, name="home"),
+    path('blog', views.blog, name="blog"),
+    path('blogpost/<str:slug>', views.blogpost, name="blog"),
+    path('contact', views.contact, name="blog"),
+    path('search', views.search, name="search"),
+]
---
+++ home/views.py
@@ -0,0 +1,50 @@
+import math
+
+from django.shortcuts import render, HttpResponse
+from home.models import Blog, Contact
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+def blog(request):
+    no_of_posts=3
+    page=request.GET.get('page')
+    if page is None:
+        page=1
+    else:
+        page=int(page)
+
+    blogs = Blog.objects.all()
+    length=len(blogs)
+    no_of_pages= math.ceil(length/no_of_posts)
+    print(no_of_pages)
+    blogs=blogs[(page-1)*no_of_posts:page*no_of_posts]
+
+    if page>1:
+        prev=page-1
+    else:
+        prev=None
+
+    if page<math.ceil(length/no_of_posts):
+        nxt=page+1
+    else:
+        nxt=None
+    context = {'blogs': blogs, 'prev':prev, 'nxt':nxt, 'no_of_pages':no_of_pages, }
+    return render(request, 'blog.html', context)
+def contact(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        phone=request.POST.get('phone')
+        email=request.POST.get('email')
+        desc=request.POST.get('desc')
+
+        instance=Contact(name=name,phone=phone,email=email,desc=desc)
+        instance.save()
+    return render(request, 'contact.html')
+def blogpost(request,slug):
+    blog= Blog.objects.filter(slug=slug).first()
+    context = {'blog': blog}
+    return render(request, 'blogpost.html',context)
+def search(request):
+    return render(request, 'search.html')
